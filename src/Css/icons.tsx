213afef166947d94bf0bd11ed718a3a3ee5b18c1
youngduck <dudejr5839@naver.com>
2023-04-27 22:47:31 +0900

    add: GlobalStyle,icon,variables

---
+++ src/Css/icons.tsx
@@ -0,0 +1,14 @@
+import { MdError } from "react-icons/md";
+import { TiArrowBack } from "react-icons/ti";
+import { GiMusicalNotes } from "react-icons/gi";
+import { BsSpotify } from "react-icons/bs";
+import { SlUserUnfollow } from "react-icons/sl";
+const icons = {
+  notice: <MdError />,
+  back: <TiArrowBack />,
+  music: <GiMusicalNotes />,
+  spotify: <BsSpotify />,
+  unfollow: <SlUserUnfollow />,
+};
+
+export default icons;
---
+++ src/Css/variables.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  //  PRIMARY COLOR
+  deepGold: "#FFBD13",
+  darkGold: "#FFD058",
+  defaultGold: "#FFF49B",
+  lightGold: "#FFFBDE",
+  //  SECONDARY COLOR
+  deepPurple: "#5326D5",
+  darkPurple: "#5734BA",
+  defaultPurple: "#6B50B9",
+  lightPurple: "#937CD4",
+  //  EMPHASIZE COLOR
+  deepPink: "#D3008C",
+  darkPink: "#EA28C5",
+  defaultPink: "#FF66E2",
+  lightPink: "#FF94CE",
+  //  GRAY COLOR
+  deepGray: "#585858",
+  darkGray: "#787878",
+  defaultGray: "#BABABA",
+  lightGray: "#EAEAEA",
+  // SPOTIFY
+  spotifyGreen: "#1DB954",
+  spotifyLightGreen: "#1ED760",
+  //Background
+  background: "#252525",
+};
