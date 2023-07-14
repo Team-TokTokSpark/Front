c44813e9606486a638519a1ae8b464f19bdf8d79
Ariling <97501226+Ariling@users.noreply.github.com>
2023-07-14 15:31:37 +0900

    main페이지 routing 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,20 +3,35 @@ import LoginPage from "./Pages/LoginPage";
 import KakaoLoginRedirect from "./Pages/KakaoLoginRedirect";
 import SignUpPage from "./Pages/SignUpPage";
 import NotFoundPage from "./Pages/NotFound";
-import HomePage from "./Pages/main";
+import HomePage from "./Pages/HomePage";
 import MusicPage from "./Pages/page";
 import MymusicPage from "./Pages/myMusicPage";
 import OthermusicPage from "./Pages/otherMusicPage";
 import FriendsListPage from "./Pages/FrinedsListPage";
 import SettingPage from "./Pages/SettingPage";
 import EditPage from "./Pages/Edit";
+import MakePage from "./Pages/MakePage";
+import MainPage from "./Pages/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
   },
-  { path: "main", element: <HomePage /> },
+  {
+    path: "main",
+    element: <MainPage />,
+    children: [
+      {
+        path: ":idx",
+        element: <HomePage />,
+      },
+      {
+        path: ":idx/makePage",
+        element: <MakePage />,
+      },
+    ],
+  },
   { path: "/oauth2/redirect", element: <KakaoLoginRedirect /> },
   { path: "signup", element: <SignUpPage /> },
   { path: "setting", element: <SettingPage /> },
