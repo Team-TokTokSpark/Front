42349ddab5e6a86b4d864645017bb548832e4351
Ariling <97501226+Ariling@users.noreply.github.com>
2023-07-14 17:05:38 +0900

    보관함 페이지 생성 일부 완성

--- src/Pages/MakePage.tsx
+++ src/Pages/MakePage.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ColorChangeDiv,
+  EditButtonDiv,
+  RemoveTextButton,
+} from "../Styles/EditPageStyle";
+import { useNavigate } from "react-router-dom";
+import {
+  MakeBodyWrapper,
+  Title,
+  TitleInput,
+  TitleMakeWrapper,
+} from "../Styles/HomePageMakeStyle";
 
 const MakePage = () => {
-  return <div>MakePage</div>;
+  const navigate = useNavigate();
+  const [text, setText] = useState("Hi");
+  const [colorNum, setColorNum] = useState(0);
+  const colorArray = ["color0", "color1", "color2", "color3"];
+  const displayText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  const removeText = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setText("");
+  };
+  return (
+    <MakeBodyWrapper>
+      {/* 해당 파트는 리팩토링이 필요한 부분 취소 완료 부분!*/}
+      <EditButtonDiv>
+        <div
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          취소
+        </div>
+        <div
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          완료
+        </div>
+      </EditButtonDiv>
+      {/* <div className="editTitle-part"></div> */}
+      <TitleMakeWrapper>
+        <Title>{text}</Title>
+        {/* <InputTitle type="text" value={text} onChange={displayText} /> */}
+        <TitleInput
+          placeholder="제목을 입력하세요"
+          type="text"
+          value={text}
+          onChange={displayText}
+        ></TitleInput>
+        <RemoveTextButton
+          style={{
+            marginLeft: "-25px",
+          }}
+          onClick={removeText}
+        >
+          X
+        </RemoveTextButton>
+      </TitleMakeWrapper>
+      <Title>페이지 배경 색상</Title>
+      {/* 색깔 바꾸는 부분도 리팩토링을 해야하는 부분! */}
+      <ColorChangeDiv>
+        {colorArray.map((e, index) => {
+          if (colorNum === index) {
+            return (
+              <button
+                className={`${"checkcolor"} ${e}`}
+                onClick={() => {
+                  setColorNum(index);
+                }}
+              />
+            );
+          } else {
+            return (
+              <button
+                className={` ${e}`}
+                onClick={() => {
+                  setColorNum(index);
+                }}
+              />
+            );
+          }
+        })}
+      </ColorChangeDiv>
+    </MakeBodyWrapper>
+  );
 };
 
 export default MakePage;
---
+++ src/Styles/HomePageMakeStyle.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const MakeBodyWrapper = styled.div`
+  width: 90%;
+  height: 90%;
+  margin: auto;
+  position: relative;
+  top: 5%;
+`;
+
+export const TitleMakeWrapper = styled.div`
+  width: 310px;
+  height: 105px;
+  margin: 40px auto;
+`;
+
+export const Title = styled.div`
+  background-color: transparent;
+  border-bottom: solid 1px white;
+  width: 300px;
+  height: 24px;
+  font-weight: 700;
+  line-height: 24px;
+  font-size: 20px;
+  color: white;
+  margin: 10px auto;
+`;
+
+export const TitleInput = styled.input`
+  /* display: flex;
+  align-items: center; */
+  width: 290px;
+  padding: 10px 15px;
+  gap: 5px;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.1);
+  margin: 10px auto;
+  border-color: transparent;
+`;
